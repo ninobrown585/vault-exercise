11838dfa58e67676e2e69903384564d9658c154e
Adrian Brown <42852900+ninobrown585@users.noreply.github.com>
2024-09-26 19:35:31 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// shows the assignment od a string to  variable line
+const line = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// shows the addition of 5 and 2
+num1 = 5+5;
+// shows the multiplication of 10 and 4
+num2= 10*4;
+// shows the subtraction of 40 and 1
+num3= 40-1;
+// shows the alert message with the line and the numbers
+alert(`${line} ${num1} ${num2} ${num3}`);
